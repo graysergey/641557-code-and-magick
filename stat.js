37e29c16d3f0ba9f31f615aca7534b6f71d09e80
GraySergey <xgortx@gmail.com>
2018-11-18 17:51:54 +0200

    Переносит файл stat.js для удобной работы в browser-sync из одной папки

--- stat.js
+++ stat.js
@@ -8,5 +8,7 @@ var renderCloud = function(ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-
+window.renderStatistics = function(ctx) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
 };
