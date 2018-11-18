d7699a424388cd3245e034a4e7708c1b8e688764
GraySergey <xgortx@gmail.com>
2018-11-18 21:15:42 +0200

    Рефакторинг кода - перенос данных игроков в цыкл

--- stat.js
+++ stat.js
@@ -26,24 +26,10 @@ window.renderStatistics = function(ctx) {
   ctx.fillText('Ура вы победили!', CLOUD_X + TITLE_GAP, GAP + TITLE_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + TITLE_GAP, TITLE_GAP + FONT_GAP);
 
-  var playerIndex = '0';
-  var playerName = 'Вы';
   var players = ['Вы', 'Иван', 'Юлия'];
 
-  ctx.fillText('Вы', CLOUD_X + (BAR_GAP * 1) + (BAR_WIDTH * playerIndex), (CLOUD_HEIGHT + GAP) - TEXT_GAP);
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(CLOUD_X + (BAR_GAP * 1) + (BAR_WIDTH * playerIndex), (CLOUD_Y + CLOUD_HEIGHT) - BAR_WIDTH - BAR_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
-
-  playerIndex = 1;
-  playerName = 'Иван';
-
-  ctx.fillStyle = '#000';
-  ctx.fillText(playerName, CLOUD_X + (BAR_GAP * 2) + (BAR_WIDTH * 1), (CLOUD_HEIGHT + GAP) - TEXT_GAP);
-  ctx.fillRect(CLOUD_X + (BAR_GAP * 2) + (BAR_WIDTH * 1), (CLOUD_Y + CLOUD_HEIGHT) - BAR_WIDTH - BAR_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
-
-  playerIndex = 2;
-  playerName = 'Юлия';
-
-  ctx.fillText(playerName, CLOUD_X + (BAR_GAP * 3) + (BAR_WIDTH * 2), (CLOUD_HEIGHT + GAP) - TEXT_GAP);
-  ctx.fillRect(CLOUD_X + (BAR_GAP * 3) + (BAR_WIDTH * 2), (CLOUD_Y + CLOUD_HEIGHT) - BAR_WIDTH - BAR_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillText(players[i], CLOUD_X + (BAR_GAP * (i + 1)) + (BAR_WIDTH * i), (CLOUD_HEIGHT + GAP) - TEXT_GAP);
+    ctx.fillRect(CLOUD_X + (BAR_GAP * (i + 1)) + (BAR_WIDTH * i), (CLOUD_Y + CLOUD_HEIGHT) - BAR_WIDTH - BAR_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
+  };
 };
