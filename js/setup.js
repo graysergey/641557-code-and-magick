60516ed68e78a9480a012bfc9b3276a2e6ff9035
GraySergey <xgortx@gmail.com>
2018-11-22 23:53:11 +0200

    Добавляет массивы с данными магов

--- js/setup.js
+++ js/setup.js
@@ -29,6 +29,46 @@ var wizards = [
     coatColor: 'rgb(127, 127, 127)'
   }
 ];
+
+var names = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var surNames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var coatsColor = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var eyesColor = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
 // Создает мага из входящего массива объектов
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
