25076ac5af1e6e91989a9a014b34fb2cc0fe1de4
GraySergey <xgortx@gmail.com>
2018-12-09 19:28:01 +0200

    Выносит getMaxValue в util.js. Замыкает область видимости stat.js

--- js/util.js
+++ js/util.js
@@ -21,5 +21,23 @@
     isEscapeEvt: function (evt) {
       return evt.keyCode === ESC__KEYCODE;
     },
+
+    getMaxValue: function (arr) {
+      var maxValue = arr[0];
+
+      for (var i = 0; i < arr.length; i++) {
+        if (maxValue < arr[i]) {
+          maxValue = arr[i];
+        }
+      }
+      // Проверяет пустой ли массив, и если пустой, то запиши в него единицу
+      // и присвой нулевой индекс для максимального елемента.
+      if (arr.length === 0) {
+        arr.push(1);
+        maxValue = Math.round(arr[0]);
+      }
+
+      return maxValue;
+    }
   };
 })();
