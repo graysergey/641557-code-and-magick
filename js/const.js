41a0876d50a5950502621bfbc7fc9131633076c6
GraySergey <xgortx@gmail.com>
2018-12-10 11:32:51 +0200

    Выносит константы из Stat в модуль const

--- js/const.js
+++ js/const.js
@@ -4,7 +4,19 @@
 
   Window.const = {
     ESC__KEYCODE: 27,
-    ENTER__KEYCODE: 13
+    ENTER__KEYCODE: 13,
+    CLOUD_WIDTH: 420,
+    CLOUD_HEIGHT: 270,
+    CLOUD_X: 100,
+    CLOUD_Y: 10,
+    GAP: 10,
+    BAR_GAP: 50,
+    TITLE_GAP: 25,
+    FONT_GAP: 30,
+    TEXT_GAP: 20,
+    BAR_WIDTH: 40,
+    BAR_HEIGHT: 150,
+    INDEX_MORE: 1
   };
 
 })();
