809031126d72eb4c5665c8de40cf5abfdbe1f513
GraySergey <xgortx@gmail.com>
2018-12-13 21:17:30 +0200

    Добавляет модуль backend.js, создает в нем три функции: load, save, error

---
+++ js/backend.js
@@ -0,0 +1,88 @@
+'use strict';
+
+(function () {
+
+  var URL = {
+    load: 'https://js.dump.academy/code-and-magick/data',
+    save: 'https://js.dump.academy/code-and-magick'
+  };
+  var SUCCESS_CODE = 200;
+
+  var onError = function (errorMessage) {
+    var popup = document.createElement('div');
+    popup.style = 'margin: 0 auto; text-align: center; color: black; background-color: red; z-index: 100';
+    popup.style.position = 'absolute';
+    popup.style.left = 0;
+    popup.style.right = 0;
+    popup.style.fontSize = '35px';
+    popup.textContent = 'errorMessage';
+    document.body.insertAdjacentElement('afterbegin', popup);
+  };
+
+
+  var load = function (onLoad, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_CODE) {
+        onLoad(xhr.response);
+      } else {
+        error('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      error('Произошла ошибка соединения');
+    });
+
+    xhr.timeout = 10000;
+    xhr.addEventListener('timeout', function () {
+      error('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('GET', URL.load);
+    xhr.send();
+  };
+
+
+  var save = function (data, onLoad, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_CODE) {
+        onLoad(xhr.response);
+      } else {
+        error('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      error('Произошла ошибка соединения');
+    });
+
+    xhr.timeout = 1000;
+    xhr.addEventListener('timeout', function () {
+      error('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.open('POST', URL.save);
+    xhr.send(data);
+  };
+
+  var userDialog = document.querySelector('.setup');
+  var form = userDialog.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    save(new FormData(form), function () {
+      userDialog.classList.add('hidden');
+    });
+    evt.preventDefault();
+  });
+
+  window.backend = {
+    load: load,
+    save: save,
+    onError: onError
+  };
+
+})();
