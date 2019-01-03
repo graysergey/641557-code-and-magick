34aaf4f79b5f4d521c03881ee5aef4e03c2419b1
GraySergey <xgortx@gmail.com>
2019-01-03 11:44:04 +0200

    Убирает лишние, закоментированные участки кода в similar.js

--- js/similar.js
+++ js/similar.js
@@ -20,41 +20,16 @@
   };
 
   var namesCompare = function (left, right) {
-    // что если я запишу так?:   --> вроде бы так тоже работает
     if (left > right) {
       return 1;
-    }
-    if (left < right) {
+    } else if (left < right) {
       return -1;
+    } else {
+      return 0;
     }
-    return 0;
-
-    // вместо такой записи:  --> такая запись была по демке
-    // if (left > right) {
-    //   return 1;
-    // } else if (left < right) {
-    //   return -1;
-    // } else {
-    //   return 0;
-    // }
   };
 
   var updateWizards = function () {
-    // var sameCoatAndEyesWizards = wizards.filter(function (it) {
-    //   return it.colorCoat === coatColor && it.colorEyes === eyesColor;
-    // });
-    // var sameCoatWizards = wizards.filter(function (it) {
-    //   return it.colorCoat === coatColor;
-    // });
-    // var sameEyesWizards = wizards.filter(function (it) {
-    //   return it.colorEyes === eyesColor;
-    // });
-
-    // var filtredWizards = sameCoatAndEyesWizards.concat(sameCoatWizards).concat(sameEyesWizards).concat(wizards);
-
-    // var uniqueWizards = filtredWizards.filter(function (it, i) {
-    //   return filtredWizards.indexOf(it) === i;
-    // });
     window.render(wizards.sort(function (left, right) {
       var rankDiff = getRank(right) - getRank(left);
       if (rankDiff === 0) {
